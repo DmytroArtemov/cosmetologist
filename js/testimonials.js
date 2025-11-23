ed53c34072d047aca51bbb2232cb716e221a3149
DmytroArtemov <dimchik.artemchik94@gmail.com>
2025-11-23 19:22:06 +0200

    Firs commit

---
+++ js/testimonials.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+    });
+};
